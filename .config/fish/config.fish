975b146faac5caad9a2138f6b8f947dbb8c6d885
avn3r90 <fi9o@outlook.com>
2024-10-29 11:19:09 +0100

    kolejne aliasy

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,10 @@ end
 
 #moje aliasy
 
-alias cat="batcat"
-alias mv="mv -v"
+alias cat='batcat'
+alias mv='mv -v'
 alias cp="rsync -ah --progress"
+alias fastping='ping -c 100 -s.2'
+alias poweroff='sudo /sbin/poweroff'
+alias lu='du -sh * | sort -h' 
+alias lt='ls --human-readable --size -1 -S --classify'
