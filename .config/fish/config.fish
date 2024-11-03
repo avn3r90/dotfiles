8d8b890586ac4c8f40f5ab27b1400cb79b6f5f28
avn3r90 <fi9o@duck.com>
2024-11-03 16:08:09 +0100

    up

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,7 +7,7 @@ export EDITOR=micro
 #ogolne
 alias cat="bat"
 alias ls="eza --icons"
-alias mv="mv -v"
+alias mv="mv -va"
 alias cp="rsync -ah --progress"
 alias poweroff="sudo /sbin/poweroff"
 alias lu="du -sh * | sort -h" 
